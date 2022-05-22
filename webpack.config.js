b20856c3fd6b53a46c8fb52e9ee72de5dfdafe22
Andrew Le <s3820098@rmit.edu.vn>
2022-05-22 20:42:49 +0700

    7. Usage of LOADERS: for sass files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,15 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js', // can be an {}, with multiple entreis
   output: {
-    filename: 'damnson.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 };
